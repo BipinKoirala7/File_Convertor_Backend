0e2d70e1e96527d26dc54392db06a7d737f72f95
Bipin Koirala <bipinkoirala2061@gmail.com>
2025-12-15 09:23:07 +0545

    File Convertor Properties added

---
+++ src/main/java/com/project/FileConvertor/Model/FileConverterProperties.java
@@ -0,0 +1,15 @@
+package com.project.FileConvertor.Model;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import lombok.Data;
+
+@Data
+@ConfigurationProperties("app.file.convertor")
+public class FileConverterProperties {
+
+    // We can add other properties as needed in the porject
+
+    private String baseDir;
+    private Integer maxSize;
+}
