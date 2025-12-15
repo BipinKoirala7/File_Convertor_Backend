5c4db70bca84a3ef9ff97f465832667ac967be8c
Bipin Koirala <bipinkoirala2061@gmail.com>
2025-12-15 07:42:47 +0545

    Initial Commit

---
+++ src/main/java/com/project/FileConvertor/FileConvertorApplication.java
@@ -0,0 +1,13 @@
+package com.project.FileConvertor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileConvertorApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(FileConvertorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/FileConvertor/FileConvertorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.FileConvertor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileConvertorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
